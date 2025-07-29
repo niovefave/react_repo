480904200f4194023d6dca809d21fd6ecebf796d
Junu Park <97wnsdn@naver.com>
2025-07-29 19:48:40 +0900

    Counter05

--- app07/src/App.jsx
+++ app07/src/App.jsx
@@ -2,12 +2,13 @@ import './App.css';
 import Counter from './components/Counter';
 import Counter02 from './components/Counter02';
 import Counter04 from './components/Counter04';
+import Counter05 from './components/Counter05';
 
 function App() {
   return (
     <>
       <h1>React App07</h1>
-      <Counter04 />
+      <Counter05 />
     </>
   );
 }
