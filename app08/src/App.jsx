c1975c4f3ca5a35a9f2e846ec57c6efbfada5604
Junu Park <97wnsdn@naver.com>
2025-07-30 12:16:58 +0900

    Counter05dddd

---
+++ app08/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import BoardPage from './pages/board/BoardPage';
+import NoticeMain from './pages/notice/NoticeMain';
+import TodoPage from './pages/TodoPage';
+
+function App() {
+  return (
+    <>
+      <h1>React App08</h1>
+      <TodoPage />
+      <BoardPage />
+      <NoticeMain />
+    </>
+  );
+}
+
+export default App;
