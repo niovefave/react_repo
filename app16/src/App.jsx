3d0629eee3669c0654fc8f65eb252f22e12a970c
Junu Park <97wnsdn@naver.com>
2025-08-05 09:14:16 +0900

    RTK study

--- app16/src/App.jsx
+++ app16/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
-import Counter from './components/Counter';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import Counter02 from './components/Counter02';
 
 function App() {
   return (
     <>
       <h1>React App16</h1>
       <Provider store={store}>
-        <Counter />
+        {/* <Counter /> */}
+        <Counter02 />
       </Provider>
     </>
   );
